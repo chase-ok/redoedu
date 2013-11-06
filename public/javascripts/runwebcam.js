2967e5a144ce0448ec1c1b238aff2dc27a2e4d70
Juliana Nazare <jules.nazare@gmail.com>
2013-11-06 11:16:58 -0500

    youtube upload

--- public/javascripts/runwebcam.js
+++ public/javascripts/runwebcam.js
@@ -11,26 +11,44 @@ var widget;
 var player;
 function onYouTubeIframeAPIReady() {
 widget = new YT.UploadWidget('widget', {
-  width: 500,
-  events: {
-    'onUploadSuccess': onUploadSuccess,
-    'onProcessingComplete': onProcessingComplete
-  }
-});
+  	width: 500,
+	  	events: {
+	    	'onUploadSuccess': onUploadSuccess,
+	    	// 'onStateChange': onPending,
+	    	'onProcessingComplete': onProcessingComplete
+	  	}
+	});
 }
 
 // 4. This function is called when a video has been successfully uploaded.
 function onUploadSuccess(event) {
-alert('Video ID ' + event.data.videoId + ' was uploaded and is currently being processed.');
+	$( '#display' ).hide("slow");
+	$( '#progressWrapper' ).show("slow");
+	var url = 'http://www.youtube.com/watch?v='+event.data.videoId;
+	$.ajax({
+		type: 'PUT',
+		url: "/story/youtube/story",
+		settings: {youtubeUrl: url, tags: ["testing"]}
+	}).done(function() {
+		for ( var i = 10; i < 99; i=i+10 ){
+			var percent = i.toString()+'%';
+			$( '#bar' ).attr("aria-valuenow", i.toString()).delay(3000);;
+			$( '#bar' ).css('width',percent).delay(3000);
+		}
+	});
+	//alert('Video ID ' + event.data.videoId + ' was uploaded and is currently being processed.');
 }
+
 
 // 5. This function is called when a video has been successfully
 //    processed.
 function onProcessingComplete(event) {
-player = new YT.Player('player', {
-  height: 390,
-  width: 640,
-  videoId: event.data.videoId,
-  events: {}
-});
+	$( '#widget' ).hide("slow");
+	$( '#progressWrapper' ).hide("slow");
+	$ ( '#player' ).show("slow");
+	player = new YT.Player('player', {
+		width: 500,
+		videoId: event.data.videoId,
+		events: {}
+	});
 }
